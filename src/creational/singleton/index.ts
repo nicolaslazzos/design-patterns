18eb66c15f315e90173838489c6dee6c976fc114
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-11-25 20:54:32 -0300

    adding singleton pattern pseudocode

--- src/creational/singleton/index.ts
+++ src/creational/singleton/index.ts
@@ -9,6 +9,40 @@ all the following calls to that static method returns the cached object
 as you see, solve two problems at the same time, so it violates the Single Responsibility Problem
 */
 
-(() => {})();
+(() => {
+  class Singleton {
+    private static instance: Singleton;
+
+    private constructor() {
+      // make the constructor private to prevent direct calls from outside
+    }
+
+    // controls the access to the singleton instance
+    public static getInstance(): Singleton {
+      if (!Singleton.instance) {
+        Singleton.instance = new Singleton();
+      }
+
+      return Singleton.instance;
+    }
+
+    public someBusinessLogic() {
+      // the singletons should define some business logic that can be executed on its instance
+    }
+  }
+
+  const clientCode = () => {
+    const s1 = Singleton.getInstance();
+    const s2 = Singleton.getInstance();
+
+    if (s1 === s2) {
+      console.log("Singleton works, both variables contain the same instance.");
+    } else {
+      console.log("Singleton failed, variables contain different instances.");
+    }
+  };
+
+  clientCode();
+})();
 
 // ref: https://refactoring.guru/design-patterns/singleton
